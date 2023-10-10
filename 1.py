1e5a4d91e6f5fd703f710ae6b03c52c0da6a7ad4
gsravani2106 <147494626+gsravani2106@users.noreply.github.com>
2023-10-10 14:59:39 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+import random
+from hangman_words import word_list
+from hangman_art import stages,logo
+chosen_word=random.choice(word_list)
+lives=6
+print(f'pssst,the solution is {chosen_word}.')
+print(logo)
+display=[]
+for i in range(len(chosen_word)):
+    display.append("_")
+while "_" in display:
+    guess=input("guess a letter:").lower()
+    for i in range(len(chosen_word)):
+        letter=chosen_word[i]
+        if letter==guess:
+            display[i]=letter
+    if guess not in chosen_word:
+        lives=lives-1
+        if lives==0:
+            print("you loose")
+            exit(1)
+    print(f"{''.join(display)}")
+    print(stages[lives])
+else:
+    print("you have won")
